5116eada19aa143a6c7f0820043926a314d12d37
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-04-05 20:31:06 +0530

    added error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { json } from "body-parser";
 
 //Route Handlers
-import { signupRouter } from "./routes/signup";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
+import { signupRouter} from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 app.use(json());
@@ -15,6 +16,9 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// using error handler
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!!!");
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
-const router = express.Router();
-const { body, validationResult } = require("express-validator");
+import { body, validationResult } from "express-validator";
 
-router.get(
+const router = express.Router();
+
+router.post(
   "/api/users/signup",
   [
     body("email").isEmail().withMessage("Email must be valid"),
@@ -13,15 +14,18 @@ router.get(
   ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error("Invalid email or password");
     }
     const { email, password } = req.body;
+
     console.log("Creating a user...");
+    throw new Error("Error connecting to database");
+
     res.send({});
+
   }
 );
-
-
 
 export { router as signupRouter };
