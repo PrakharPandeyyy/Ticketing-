0907ef1144f43c2b6a027ac8747d364446641c71
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-04-05 22:33:03 +0530

    added custom errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,12 +1,26 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
-) => 
-{
-console.log('Something went wrong', err);
-res.status(400).send({message: "Something went wrong"});
-}
+) => {
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      return {
+        message: error.msg,
+        field: error.type === "field" ? error.path : "",
+      };
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
+    res.status(400).send({
+        errors: [{ message: err.message}],
+    });
+};
