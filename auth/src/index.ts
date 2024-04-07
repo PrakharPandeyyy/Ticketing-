7371b64b4a89d20a86292af8f563e51988f23f2a
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-04-07 18:58:35 +0530

    setting up sessions

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
 
 //Route Handlers
 import { currentUserRouter } from "./routes/current-user";
@@ -15,7 +17,15 @@ import { NotFoundError } from "./errors/not-found-error";
 
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
 
 // using route handlers
 app.use(currentUserRouter);
