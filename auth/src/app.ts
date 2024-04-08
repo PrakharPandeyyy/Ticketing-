16fa99a28847bdaed409b22e1cc4dfdd7032e3f1
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-04-08 14:46:23 +0530

    refactoring code for testing

---
+++ auth/src/app.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
+
+//Route Handlers
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+//Middlewares
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
+
+
+// using route handlers
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// using error handler
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,43 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-
-//Route Handlers
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-
-//Middlewares
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: false,
-  })
-);
-
-
-// using route handlers
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// using error handler
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if(!process.env.JWT_KEY){
@@ -50,7 +12,7 @@ const start = async () => {
     console.error(err);
   }
   app.listen(3000, () => {
-    console.log("Listening on port 3000!!!!!");
+    console.log("Listening on port 3000!!");
   });
 };
 start();
