e1fe4f84e9a5db18855658331d026b06b766f0f4
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-04-08 11:20:31 +0530

    setting up sessions

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -22,7 +22,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: true,
+    secure: false,
   })
 );
 
@@ -40,6 +40,9 @@ app.all("*", async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if(!process.env.JWT_KEY){
+    throw new Error('JWT_KEY must be defined')
+  }
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to MongoDB");
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { User } from "../models/users";
@@ -33,6 +34,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+  
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+    // Store it on session object
+    req.session= {
+      jwt: userJwt
+    }
+
     res.status(201).send(user);
 
     res.send({});
