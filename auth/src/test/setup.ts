f6e603c2c78d361fd6bb0133f44b6e26eddd2a7e
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-04-08 22:11:38 +0530

    test for currentuser

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,13 +1,23 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
+import request from "supertest";
+
+declare global {
+  namespace NodeJS {
+      interface Global {
+          signup(): Promise<string[]>;
+      }
+  }
+}
+
 
 let mongo: any;
 
 beforeAll(async () => {
-process.env.JWT_KEY ='ppthegreat'
-mongo = await MongoMemoryServer.create();
-const mongoUri = mongo.getUri();
+  process.env.JWT_KEY = "ppthegreat";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
 
   await mongoose.connect(mongoUri);
 });
@@ -24,3 +34,18 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
