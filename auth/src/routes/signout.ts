f63de50c7e502ff8d7f269db41b99daee042b270
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-04-08 21:50:26 +0530

    test for signout

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-router.get('/api/users/signout', (req,res) => {
+router.post('/api/users/signout', (req,res) => {
     req.session = null;
-    res.send({});
+    res.send({"message":"You are logged out"});
 
 });
 
