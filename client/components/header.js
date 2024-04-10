1baccd32389c2ff908faa094ab812309226efccc
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-04-10 15:53:08 +0530

    header added

---
+++ client/components/header.js
@@ -0,0 +1,30 @@
+function Header({ currentUser }) {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <a href={href} className="nav-link">
+            {label}
+          </a>
+        </li>
+      );
+    });
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        Ticketing APP
+      </a>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
